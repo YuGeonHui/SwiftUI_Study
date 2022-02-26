f3b41d8fa2cf4ad66520fa69de39d7cae33cc75a
YuGeonHui <ghyu_0906@naver.com>
2022-02-27 01:03:17 +0900

    Coffee AddOrder View Design

---
+++ Hello_SwiftUI/CoffeeOrderingApp/Views/AddCoffeeOrderView.swift
@@ -0,0 +1,90 @@
+//
+//  AddCoffeeOrderView.swift
+//  Hello_SwiftUI
+//
+//  Created by geonhui Yu on 2022/02/27.
+//
+
+import SwiftUI
+import Combine
+
+struct AddCoffeeOrderView: View {
+    
+    @ObservedObject private var addCoffeeOrderVM = AddCoffeeOrderViewModel()
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            VStack {
+                
+                Form {
+                    
+                    Section(header: Text("INFORMATION").font(.body)) {
+                        TextField("Enter Name", text: self.$addCoffeeOrderVM.name)
+                    }
+                    
+                    Section(header: Text("SELECT COFFEE").font(.body)){
+                        
+                        ForEach(addCoffeeOrderVM.coffeeList, id: \.name) { coffee in
+                            CoffeeCellView(coffee: coffee, selection: self.$addCoffeeOrderVM.coffeeName)
+                        }
+                    }
+                    
+                    Section(header: Text("SELECT COFFEE").font(.body), footer: Text("Total")) {
+                        
+                        Picker("", selection: self.$addCoffeeOrderVM.size) {
+                            Text("Small").tag("Small")
+                            Text("Medium").tag("Medium")
+                            Text("Large").tag("Large")
+                        }.pickerStyle(SegmentedPickerStyle())
+                    }
+                }
+                
+                HStack {
+                    Button("Place Order") {
+                        
+                    }
+                }
+                .padding(EdgeInsets(top: 12, leading: 100, bottom: 12, trailing: 100))
+                .foregroundColor(Color.white)
+                .background(Color(red: 46/255, green: 204/255, blue: 113/255))
+                .cornerRadius(10)
+            }
+            
+            .navigationBarTitle("Add Order")
+        }
+    }
+}
+
+struct CoffeeCellView: View {
+
+    let coffee: CoffeeViewModel
+    @Binding var selection: String
+    
+    var body: some View {
+        
+        HStack {
+            Image(coffee.imageURL).resizable()
+                .frame(width: 100, height: 100)
+                .cornerRadius(16)
+            Text(coffee.name)
+                .font(.title)
+                .padding([.leading], 20)
+            
+            Spacer()
+            
+            Image(systemName: self.selection == self.coffee.name ?
+                  "checkmark" : "").padding()
+        }.onTapGesture {
+            self.selection = self.coffee.name
+            
+        }
+    }
+}
+
+struct AddCoffeeOrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddCoffeeOrderView()
+    }
+}
--- Hello_SwiftUI/ContentView.swift
+++ Hello_SwiftUI/ContentView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        WeatherMainView()
+        AddCoffeeOrderView()
 //        
 //        VStack(alignment: .center) {
 //            
