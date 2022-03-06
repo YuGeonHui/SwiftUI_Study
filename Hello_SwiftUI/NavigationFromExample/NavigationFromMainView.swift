618df09bedae4c74a28d9af207222c542a774d49
YuGeonHui <ghyu_0906@naver.com>
2022-03-07 00:22:14 +0900

    NavigationBar & Section Study

--- Hello_SwiftUI/NavigationFromExample/NavigationFromMainView.swift
+++ Hello_SwiftUI/NavigationFromExample/NavigationFromMainView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct NavigationFromMainView: View {
+    
+    @State private var images = ["😍", "😍", "😍", "😍", "😍", "😍", "😍", "😍", "😍", "😍"]
+    
     var body: some View {
        
         NavigationView {
@@ -16,8 +19,38 @@ struct NavigationFromMainView: View {
                 
                 Section(header: Text("")) {
                     
-                    
+                    HStack {
+                        
+                        Text("Show Previews")
+                        Spacer()
+                        
+                        NavigationLink(destination: Text("")) {
+                            Text("Always").foregroundColor(.gray)
+                        }.fixedSize()
+                    }
                 }
+                
+                Section(header: Text("Notifciation previews will bve shown whetehr the ipone locked or unlocked").padding().lineLimit(nil)) {
+                    
+                    NavigationLink(destination: Text("")) {
+                        Text("Siri Suggestions")
+                    }
+                }
+                
+                Section(header: VStack(alignment: .leading) {
+                    Text("Choose while apps can suggest shortcuts on the lock screen").lineLimit(nil).padding(2)
+                    Text("NOTIFICATION STYLE").padding(2)
+                    
+                }) {
+                    ForEach(self.images, id: \.self) { img in
+                        
+                        HStack {
+                            Text(img).font(.largeTitle)
+                            Text("Notification")
+                        }
+                    }
+                }
+                .navigationTitle("Notifications")
             }
         }
     }
