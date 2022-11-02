ce4909f85b7a861b7c76bf7fc57ed7cfdb306fc0
YuGeonHui <ghyu_0906@naver.com>
2022-11-02 17:22:17 +0900

    Stock Main View 샘픎 모델 설정

---
+++ Hello_SwiftUI/Stock/StockView.swift
@@ -0,0 +1,21 @@
+//
+//  StockView.swift
+//  Hello_SwiftUI
+//
+//  Created by geonhui Yu on 2022/11/02.
+//
+
+import SwiftUI
+
+struct StockView: View {
+    
+    var body: some View {
+        Text("Stock View")
+    }
+}
+
+struct StockView_Previews: PreviewProvider {
+    static var previews: some View {
+        StockView()
+    }
+}
