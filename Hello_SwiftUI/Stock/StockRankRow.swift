fe2a0c7a4822d00f5408057f2be5064e8f25b878
YuGeonHui <ghyu_0906@naver.com>
2022-11-02 18:08:27 +0900

    StockRowView 추가

---
+++ Hello_SwiftUI/Stock/StockRankRow.swift
@@ -0,0 +1,22 @@
+//
+//  StockRankRow.swift
+//  Hello_SwiftUI
+//
+//  Created by geonhui Yu on 2022/11/02.
+//
+
+import SwiftUI
+
+struct StockRankRow: View {
+    
+    var body: some View {
+       
+        Text("123132")
+    }
+}
+
+struct StockRankRow_Previews: PreviewProvider {
+    static var previews: some View {
+        StockRankRow()
+    }
+}
