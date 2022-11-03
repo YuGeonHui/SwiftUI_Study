87cc438c5197b89958b09d15f9a2626826786a2f
syss220211 <syss220211@gmail.com>
2022-11-03 22:58:45 +0900

    Stock Row 개발 완료

--- Hello_SwiftUI/Stock/StockRankRow.swift
+++ Hello_SwiftUI/Stock/StockRankRow.swift
@@ -9,14 +9,57 @@ import SwiftUI
 
 struct StockRankRow: View {
     
+    var stock: StockModel
+    
     var body: some View {
        
-        Text("123132")
+        HStack {
+            
+            Text("\(stock.rank)").font(.system(size: 16, weight: .bold))
+                .frame(width: 30)
+                .foregroundColor(.blue)
+            
+            Image("spicy_icon")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                
+                Text("\(stock.name)")
+                    .font(.system(size: 12))
+                    .foregroundColor(.white)
+            
+                HStack {
+                    
+                    Text("\(stock.price) 원")
+                        .font(.system(size: 12))
+                        .foregroundColor(.gray)
+                    
+                    Text("\(stock.diff * 100, specifier: "%.2f") %")
+                        .font(.system(size: 12))
+                        .foregroundColor(stock.diff > 0 ? .red : .blue)
+                }
+            }
+            
+            Spacer()
+            
+            Image(systemName: "heart.fill")
+                .resizable()
+                .renderingMode(.template)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+                .foregroundColor(.gray)
+            
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(.black)
     }
 }
 
 struct StockRankRow_Previews: PreviewProvider {
     static var previews: some View {
-        StockRankRow()
+        StockRankRow(stock: StockModel.list[0])
     }
 }
